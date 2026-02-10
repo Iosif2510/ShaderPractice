965b4c23b82c8bdb93247790a07b3bbb445d7d38
Iosif2510 <gpotatok@gmail.com>
2026-02-11 08:37:19 +0900

    feat: Mesh Outline 0.2.0

---
+++ Packages/com.2510.simplemeshoutline/Runtime/Editor/InlineToggleAttribute.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _2510.SimpleMeshOutline.Editor
+{
+    public class InlineToggleAttribute : PropertyAttribute
+    {
+        public readonly string ToggleFieldName;
+        public readonly float Min;
+        public readonly float Max;
+        public readonly bool UseRange;
+        
+        public InlineToggleAttribute(string toggleFieldName)
+        {
+            ToggleFieldName = toggleFieldName;
+            UseRange = false;
+        }
+        
+        public InlineToggleAttribute(string toggleFieldName, float min, float max)
+        {
+            ToggleFieldName = toggleFieldName;
+            Min = min;
+            Max = max;
+            UseRange = true;
+        }
+    }
+}
---
+++ Packages/com.2510.simplemeshoutline/Runtime/Editor/InlineToggleDrawer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace _2510.SimpleMeshOutline.Editor
+{
+    [CustomPropertyDrawer(typeof(InlineToggleAttribute))]
+    public class InlineToggleDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var attr = (InlineToggleAttribute)attribute;
+            string path = property.propertyPath;
+            int lastDot = path.LastIndexOf('.');
+            string parentPath = lastDot != -1 ? path.Substring(0, lastDot + 1) : "";
+            var toggleProp = property.serializedObject.FindProperty(parentPath + attr.ToggleFieldName);
+
+            var container = new VisualElement { style = { flexDirection = FlexDirection.Row, marginBottom = 2 } };
+
+            if (toggleProp != null && toggleProp.propertyType == SerializedPropertyType.Boolean)
+            {
+                // 1. 토글 생성
+                var toggleField = new PropertyField(toggleProp, "");
+                toggleField.style.width = 20;
+                toggleField.style.marginRight = 5;
+
+                // 2. 데이터 필드 생성 (Range 여부에 따라 분기)
+                VisualElement dataField;
+                if (attr.UseRange && property.propertyType == SerializedPropertyType.Float)
+                {
+                    // 슬라이더 생성 및 숫자 입력칸 활성화
+                    var slider = new Slider(property.displayName, attr.Min, attr.Max)
+                    {
+                        bindingPath = property.propertyPath,
+                        showInputField = true // 이 설정이 숫자 입력칸을 나타나게 합니다.
+                    };
+                    slider.labelElement.style.minWidth = 120; // 라벨 너비 확보
+                    dataField = slider;
+                }
+                else
+                {
+                    dataField = new PropertyField(property, property.displayName);
+                }
+                
+                dataField.style.flexGrow = 1;
+
+                // 3. 활성화 상태 동기화
+                void UpdateEnabled(bool val) => dataField.SetEnabled(val);
+                UpdateEnabled(toggleProp.boolValue);
+                toggleField.RegisterValueChangeCallback(evt => UpdateEnabled(evt.changedProperty.boolValue));
+
+                container.Add(toggleField);
+                container.Add(dataField);
+            }
+            return container;
+        }
+    }
+}
